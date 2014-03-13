ea6120aa252774d886c47c270de8731451977268
Tiago de Assis Gonçalves <contato@tiagogoncalves.com.br>
2014-03-13 19:27:05 -0300

    Fix test address.

--- test/index_test.js
+++ test/index_test.js
@@ -1,13 +1,13 @@
 describe('scup index', function() {
 
   it('should have data', function() {
-    browser.get('http://localhost/#/');
+    browser.get('http://localhost:9876');
 
     expect(element(by.css('.clients-list')).isPresent()).toBeTruthy();
   });
 
   it('should have error message', function() {
-    browser.get('http://localhost/#/?error=1');
+    browser.get('http://localhost:9876/#/?error=1');
 
     expect(element(by.css('.error')).isPresent()).toBeTruthy();
   });
